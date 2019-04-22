fae791b2893c571f353d8fc50e8c70472773c08f
Davert <davert.ua@gmail.com>
2019-04-23 01:16:38 +0300

    updated tests

--- tests/seeder/DatabaseSeederCest.php
+++ tests/seeder/DatabaseSeederCest.php
@@ -6,11 +6,13 @@ class DatabaseSeederCest
 {
     public function firstTest(SeederTester $I)
     {
-        $I->assertCount(1, User::all());
+        $users = User::all()->toArray();
+        $I->assertCount(1, $users);
     }
 
     public function secondTest(SeederTester $I)
     {
-        $I->assertCount(1, User::all());
+        $users = User::all()->toArray();
+        $I->assertCount(1, $users);
     }
 }
